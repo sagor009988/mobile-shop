4b9832c6ea789a2a1581201a01ee9c576e9686c3
sagor_hossain <soheltashjid001@gmail.com>
2024-12-23 23:26:04 +0600

    server side Done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
-var cors = require('cors');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 
@@ -10,10 +11,40 @@ const port = process.env.PORT || 4000;
 // middleware
 app.use(cors(
     {
-        origin: "http://localhost:5173"
+        origin: "http://localhost:5173",
+        optionsSuccessStatus: 200,
     }
 ));
 app.use(express.json());
+
+// token verification
+
+const verifyJwt = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.send({ message: "No token" })
+    }
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_KEY_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.send({ message: "Invalid token" })
+        }
+        req.decoded = decoded;
+        next()
+    })
+};
+
+// verify seller
+
+const verifySeller = async (req, res, next) => {
+    const email = req.decoded.email;
+    const query = { email: email };
+    const user = await userCollection.findOne(query);
+    if (user?.role !== "Seller") {
+        return req.send({ message: "Forbidden Access" });
+    }
+    next();
+}
 
 
 // mongodb connect
@@ -28,11 +59,104 @@ const client = new MongoClient(url, {
     }
 });
 
+// collection
+
+const userCollection = client.db("mobileShop").collection("users");
+const productCollection = client.db("mobileShop").collection("products");
+
 async function connectToDatabase() {
     try {
 
         await client.connect(); // Connect to MongoDB
         console.log('Connected to MongoDB successfully');
+
+        // get user
+        app.get("/users/:email", async (req, res) => {
+            const query = { email: req.params.email };
+
+            const user = await userCollection.findOne(query);
+            if (!user) {
+                return res.send({ message: "no user found" })
+            }
+            return res.send(user);
+        })
+
+
+        // insert User
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: "User is already exist" });
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result)
+        });
+
+
+
+        // add products
+        app.post("/add-products", verifyJwt, verifySeller, async (req, res) => {
+            const product = req.body;
+            const result = await productCollection.insertOne(product);
+            res.send(result);
+        });
+
+        // get products
+
+        app.get("/all-products", async (req, res) => {
+            // name search 2.sort by price 3.filter by category 4.filter by brand
+            const { title, sort, category, brand, page = 1, limit = 9 } = req.query;
+
+            const query = {};
+            if (title) {
+                query.title = { $regex: title, $options: "i" }
+            }
+            if (category) {
+                query.category = { $regex: category, $options: "i" }
+            }
+            if (brand) {
+                query.brand = brand
+            }
+
+            const sortOption = sort == "asc" ? 1 : -1;
+
+            const pageNumber = Number(page);
+            const limitNumber = Number(limit);
+
+            const products = await productCollection
+                .find(query)
+                .skip((pageNumber - 1) * limit)
+                .limit(limitNumber)
+                .sort({ price: sortOption })
+                .toArray();
+
+            // total products
+
+            const totalProducts = await productCollection.countDocuments(query);
+
+            // brand and category differ from all
+
+            // const productsInfo = await productCollection.find({}, { projection: { category: 1, brand: 1 } }).toArray();
+
+            const categories = [...new Set(products.map((product) => (product.category)))];
+            const brands = [...new Set(products.map((product) => (product.brand)))];
+
+            res.json({ products, categories, brands, totalProducts });
+
+        });
+
+        // my products
+        app.get("/products", async (req, res) => {
+            try {
+                const products = await productCollection.find().toArray();
+                res.send(products);
+            } catch (err) {
+                console.error('Error fetching products:', err.message);
+                res.status(500).send({ message: "Failed to fetch products" });
+            }
+        });
 
 
     } catch (err) {
@@ -50,6 +174,13 @@ connectToDatabase();
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+});
+
+//  jwt
+app.post("/authentication", async (req, res) => {
+    const userEmail = req.body;
+    const token = jwt.sign(userEmail, process.env.ACCESS_KEY_TOKEN, { expiresIn: '10d' });
+    res.send({ token });
 })
 
 app.listen(port, () => {
